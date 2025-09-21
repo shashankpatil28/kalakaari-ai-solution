8097863c0c2b63128dfef0a0ac672051b8d4235e
shashankpatil28 <patilshashank2003@gmail.com>
2025-09-21 17:13:49 +0530

    added cors

--- master-ip/server/app/main.py
+++ master-ip/server/app/main.py
@@ -3,6 +3,7 @@ import os
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 from app.mongodb import MongoDB
 from app.routes.craftid import router as craftid_router
 
@@ -23,6 +24,21 @@ async def lifespan(app: FastAPI):
         MongoDB.close()
 
 app = FastAPI(title="Master-IP Prototype Service", version="0.1.0", lifespan=lifespan)
+
+# CORS settings
+origins = [
+    "http://localhost:4200",
+    "https://kalakari-ai-solution-shop-production.up.railway.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # specific origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(craftid_router)
 @app.get("/")
